469aa8d0fbe4d62cc98d6a932f97b2f5e15cc5d9
Tim Pilius <tpilius@gmail.com>
2023-09-18 21:02:57 -0400

    asd

--- LancacheChunkPurge/Properties/GlobalUsings.cs
+++ LancacheChunkPurge/Properties/GlobalUsings.cs
@@ -1,23 +1,8 @@
-﻿global using AutoMapper;
-global using HexMate;
-global using JetBrains.Annotations;
-global using ProtoBuf;
-global using System;
-global using System.Collections.Concurrent;
+﻿global using System;
 global using System.Collections.Generic;
 global using System.Diagnostics;
 global using System.Diagnostics.CodeAnalysis;
-global using System.IO;
 global using System.Linq;
-global using System.Net;
-global using System.Net.Http;
-global using System.Runtime.Serialization;
-global using System.Security.Cryptography;
-global using System.Text.Json;
 global using System.Text.RegularExpressions;
-global using System.Threading.Tasks;
 global using System.Text;
 global using ByteSizeLib;
-
-
-
